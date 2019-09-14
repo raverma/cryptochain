2e04dda2d69e40597ccd740c611e07d2ebab913d
Rahul Verma <verma.rahul32@gmail.com>
2019-09-14 19:40:07 +0530

    Added redis package and code to publish message on channel

---
+++ index.js
@@ -0,0 +1,28 @@
+const express = require('express');
+const bodyParser = require('body-parser');
+const BlockChain = require('./blockchain');
+const PubSub = require('./pubsub');
+
+const app = express();
+const blockchain = new BlockChain();
+const pubsub = new PubSub({blockchain});
+setTimeout(() => pubsub.broadcastChain(), 1000);
+
+app.use(bodyParser.json());
+
+app.get('/api/blocks', (req, res) => {
+    res.json(blockchain.chain);
+});
+
+app.post('/api/mine', (req, res)=> {
+    const { data } = req.body;
+
+    blockchain.addBlock({data});
+
+    res.redirect('/api/blocks');
+});
+const PORT = 3000;
+app.listen(PORT, ()=> {
+    console.log(`listening at localhost:${PORT}`);
+});
+
